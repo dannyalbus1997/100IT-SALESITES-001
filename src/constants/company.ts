e903921f688e1b1bf8dc832dc2f5f44fb6e26b6b
talhawajdan <134398365+talhawajdandanny@users.noreply.github.com>
2026-02-27 15:32:19 +0500

    #update

--- src/constants/company.ts
+++ src/constants/company.ts
@@ -1,11 +1,11 @@
 export const COMPANY_DETAILS = {
   name: "Assure Supported Living",
-  address: "71-75 Shelton Street, Covent Garden, London, WC2H 9JQ",
-  crn: "12345678",
-  phone: "+44 20 7946 0000",
+  address: "-",
+  crn: "-",
+  phone: "-",
   email: "info@assuresupportedliving.co.uk",
-  hours: "Mon - Fri: 09:00 - 17:30",
-  vat: "GB 123 4567 89",
+  hours: "-",
+  vat: "-",
 };
 
 export const NAV_LINKS = [
